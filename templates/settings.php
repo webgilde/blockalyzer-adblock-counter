d943099954effc882e7f952a379e1b405fe8aa70
webzunft <post@webzunft.de>
2013-02-15 21:37:28 +0100

    show start and stop time in backend when set

--- templates/settings.php
+++ templates/settings.php
@@ -9,15 +9,24 @@
 </div>
 <div class="abc-form-block">
     <form action="" method="post">
+        <?php if ( $this->_is_measuring() ) : ?>
+        <span><?php echo get_option('abc_start'); ?></span>
+        <?php else : ?>
         <input type="hidden" name="abcounter" value="start"/>
         <input type="submit" value="<?php _e('start measuring', ABCOUNTERDIR ); ?>"/>
+        <?php endif; ?>
     </form>
     <p><?php _e('Starts the measuring (again). Sets the start time.', ABCOUNTERTD); ?></p>
 </div>
 <div class="abc-form-block">
     <form action="" method="post">
+        <?php $stop = get_option('abc_stop'); 
+        if ( !empty( $stop ) ) : ?>
+        <span><?php echo get_option('abc_stop'); ?></span>
+        <?php else : ?>
         <input type="hidden" name="abcounter" value="stop"/>
         <input type="submit" value="<?php _e('stop measuring', ABCOUNTERDIR ); ?>"/>
+        <?php endif; ?>
     </form>
     <p><?php _e('Stop the measuring. Sets the end time.', ABCOUNTERTD); ?></p>
 </div>
