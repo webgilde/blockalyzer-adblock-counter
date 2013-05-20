f83914ac688046408e4f5f88868e50e4293372fd
webzunft <post@webzunft.de>
2013-05-20 10:49:44 +0200

    added hash to tracking and new adblock stats

--- classes/tracking.php
+++ classes/tracking.php
@@ -1,6 +1,7 @@
-<?php 
-if ( !class_exists( 'ABCOUNTER_CLASS' ) ) {
-    header( 'HTTP/1.0 403 Forbidden' );
+<?php
+
+if (!class_exists('ABCOUNTER_CLASS')) {
+    header('HTTP/1.0 403 Forbidden');
     die;
 }
 
@@ -9,41 +10,50 @@ if ( !class_exists( 'ABCOUNTER_CLASS' ) ) {
  * @since 1.2
 
  */
-if ( !class_exists( 'ABC_Tracking' ) ) {
+if (!class_exists('ABC_Tracking')) {
+
     class ABC_Tracking {
-        
+
         /**
          * send blog adblock data
          */
         static function send() {
-            
+
+            $hash = get_option('abc_tracking_hash');
+
+            if (empty($hash)) {
+                $hash = md5( site_url() );
+                update_option('abc_tracking_hash', $hash);
+            }
+
             $data = array(
                 'site' => array(
-                    'url'       => site_url(),
-                    'name'      => get_bloginfo( 'name' ),
-                    'lang'      => get_locale(),
-                ),                
+                    'hash' => $hash,
+                    'url' => site_url(),
+                    'name' => get_bloginfo('name'),
+                    'lang' => get_locale(),
+                ),
                 'stats' => array(
-                    'last_reset'=> get_option('abc_last_reset', 0),
-                    'views'     => get_option('abc_page_views', 0),
-                    'users'     => get_option('abc_unique_visitors', 0)
+                    'last_reset' => get_option('abc_last_reset', 0),
+                    'views' => get_option('abc_page_views', 0),
+                    'users' => get_option('abc_unique_visitors', 0),
+                    'views_blocked' => get_option('abc_page_views_blocked', 0),
+                    'users_blocked' => get_option('abc_unique_visitors_blocked', 0)
                 )
             );
-            
+
             $args = array(
                 'body' => $data
             );
-            $result = wp_remote_post( 'http://stats.blockalyzer.com/', $args );
-            
-            if ( 200 == $result['response']['code'] ) {
-                
-                $return = json_decode( $result['body'] );
-                print_r( $return->site->url );
-                
-                
-                
+            $result = wp_remote_post('http://stats.blockalyzer.com/', $args);
+
+            if (200 == $result['response']['code']) {
+
+                $return = json_decode($result['body']);
+                print_r($return->site->url);
             }
-            
         }
+
     }
+
 }
