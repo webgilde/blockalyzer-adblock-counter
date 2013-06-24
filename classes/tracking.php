fc52b2fab5c8d0e558abdc98d94cb31b4f6af34c
webzunft <post@webzunft.de>
2013-06-24 16:32:46 +0200

    fixes #33

--- classes/tracking.php
+++ classes/tracking.php
@@ -17,6 +17,7 @@ if (!class_exists('BA_Tracking')) {
         /**
          * send blog adblock data and compare with data from others
          * @param array site options
+         * @updated 1.2.5
          */
         static function compare( $siteoptions = array() ) {
 
@@ -26,14 +27,26 @@ if (!class_exists('BA_Tracking')) {
                 $hash = md5( site_url() );
                 update_option('ba_tracking_hash', $hash);
             }
+            
+            $locales = get_bloginfo('language');
+            $lang = '';
+            $locale = '';
+                
+            if (!empty($locales)) {
+                $locales = explode('-', $locales);
+                if ( count( $locales ) >= 2 ) {
+                    $lang = $locales[0];
+                    $locale = $locales[1];
+                }
+            } 
 
             $data = array(
                 'site' => array(
                     'hash' => $hash,
                     'url' => site_url(),
                     'name' => '', //get_bloginfo('name'),
-                    'lang' => get_locale(),
-                    'country' => '',
+                    'lang' => $lang,
+                    'locale' => $locale,
                     'category' => $siteoptions['benchmark_category'],
                 ),
                 'stats' => array(
